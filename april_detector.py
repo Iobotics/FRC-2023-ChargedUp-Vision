957d7c8f882e05fdc64622b2f54b306291c2e1fa
leungbrayden <leungbrayden@gmail.com>
2023-02-06 19:25:17 -1000

    apriltags working

--- april_detector.py
+++ april_detector.py
@@ -12,7 +12,7 @@ class AprilDetector:
         # Copying parameters from the calibration script result
         fx, fy, cx, cy = (1395.1527042544735, 1391.6280089009383, 639.070620047402, 353.97356161241004)
         # creates an ApriltagPoseEstimator to calculate the position, angle, distance, etc of apriltag
-        config = robotpy_apriltag.AprilTagPoseEstimator.Config(6, fx, fy, cx, cy)
+        config = robotpy_apriltag.AprilTagPoseEstimator.Config(0.153 , fx, fy, cx, cy)
         self.estimator = robotpy_apriltag.AprilTagPoseEstimator(config)
 
         # adds a family of apriltags
@@ -22,8 +22,6 @@ class AprilDetector:
         return cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
     def detect(self,frame):
-        translate = self.estimator.estimate(i).translation()
-        rotation = self.estimator.estimate(i).rotation()
         processed = self.convert(frame)
         #cts = contours(processed)
         #with_labels = draw(frame,  cts)
@@ -49,25 +47,25 @@ class AprilDetector:
                 #print(order)
 
                 # draw lines from the corner points
-                cv2.line(processed, order[0,:].astype(int), order[1,:].astype(int), (0,0,255), 3)
-                cv2.line(processed, order[1,:].astype(int), order[2,:].astype(int), (0,0,255), 3)
-                cv2.line(processed, order[2,:].astype(int), order[3,:].astype(int), (0,0,255), 3)
-                cv2.line(processed, order[3,:].astype(int), order[0,:].astype(int), (0,0,255), 3)
+                cv2.line(frame, order[0,:].astype(int), order[1,:].astype(int), (0,0,255), 3)
+                cv2.line(frame, order[1,:].astype(int), order[2,:].astype(int), (0,0,255), 3)
+                cv2.line(frame, order[2,:].astype(int), order[3,:].astype(int), (0,0,255), 3)
+                cv2.line(frame, order[3,:].astype(int), order[0,:].astype(int), (0,0,255), 3)
 
                 # converts float number to int to plot a circle on the corner
-                cv2.circle(processed, (tuple(order[0,:].astype(int))), 8, (255,100,0), 4) # left bottom
-                cv2.circle(processed, (tuple(order[1,:].astype(int))), 8, (255,100,0), 4) # right bottom
-                cv2.circle(processed, (tuple(order[2,:].astype(int))), 8, (255,100,0), 4) # right top
-                cv2.circle(processed, (tuple(order[3,:].astype(int))), 8, (255,100,0), 4) # left top
+                cv2.circle(frame, (tuple(order[0,:].astype(int))), 8, (255,100,0), 4) # left bottom
+                cv2.circle(frame, (tuple(order[1,:].astype(int))), 8, (255,100,0), 4) # right bottom
+                cv2.circle(frame, (tuple(order[2,:].astype(int))), 8, (255,100,0), 4) # right top
+                cv2.circle(frame, (tuple(order[3,:].astype(int))), 8, (255,100,0), 4) # left top
 
                 #cv2.putText(capture, "ID", tuple(order[1,:].astype(int)), font, 2, (255,200,150), 5, cv2.LINE_AA, False)
                 # AprilTagPoseEstimator
                 #pose = estimator.estimate(det)
                 # Print the estimated pose
-                print("Pose Estimator: ", self.estimator.estimate(i))
-                print(self.estimator.estimate(i).translation())
-                print(self.estimator.estimate(i).rotation())
+                translate = self.estimator.estimate(i).translation()
+                rotation = self.estimator.estimate(i).rotation()
+                return [translate.X(),translate.Y(),translate.Z(), rotation.X(),rotation.Y(),rotation.Z()], frame
             else:
                 # shows the image with the circles and lines on
-                return (translate.X(),translate.Y(),translate.Z()), (rotation.X(),rotation.Y(),rotation.Z()), processed
-        return 0, processed
+                return 0, frame
+        return 0, frame
--- cameraServer.py
+++ cameraServer.py
@@ -3,7 +3,7 @@ import ntcore
 import cv2
 import numpy as np
 import time
-import AprilDetector
+import april_detector
 
 def contours(binary_img, min_area=15):
     contour_list, _ = cv2.findContours(binary_img, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
@@ -43,23 +43,23 @@ def draw(img, contour_list, drawColor):
 #     return output_img
 
 def main():
-    april_detector = AprilDetector()
+    april = april_detector.AprilDetector()
 
     cameraSelected = 0
     cs = cscore.CameraServer
     cs.enableLogging()
 
     camera_1 = cscore.UsbCamera(name = 'camera_1', dev = 0)
-    camera_1.setResolution(320,240)
+    camera_1.setResolution(1280,960)
     camera_2 = cscore.UsbCamera(name = 'camera_2', dev = 2)
-    camera_2.setResolution(320,240)
+    camera_2.setResolution(1280,960)
 
     cs.addCamera(camera_1)
     cs.addCamera(camera_2)
 
     # Does not actually send video back to dashboard
     # Creates cvSource object that is used to send frames back
-    outputSource = cs.putVideo('feed',320,240)
+    outputSource = cs.putVideo('feed',1280,960)
 
     #creates cvSink object that can be used to grab frames
     outputSink1 = cs.getVideo(camera=camera_1)
@@ -76,12 +76,20 @@ def main():
     nt.startClient4("temp")
 
     sd = nt.getTable("SmartDashboard")
-
-    rotation = sd.IntegerArrayTopic("Rotation")
-    translation = sd.IntegerArrayTopic("Translation")
-
-    rotationPublisher = rotation.publish()
-    translationPublisher = translation.publish()
+    
+    translationX = sd.getDoubleTopic("TranslationX")
+    translationY = sd.getDoubleTopic("TranslationY")
+    translationZ = sd.getDoubleTopic("TranslationZ")
+    rotationX = sd.getDoubleTopic("RotationX")
+    rotationY = sd.getDoubleTopic("RotationY")
+    rotationZ = sd.getDoubleTopic("RotationZ")
+    
+    translationXPublisher = translationX.publish()
+    translationYPublisher = translationY.publish()
+    translationZPublisher = translationZ.publish()
+    rotationXPublisher = rotationX.publish()
+    rotationYPublisher = rotationY.publish()
+    rotationZPublisher = rotationZ.publish()
     while True:
         # Grabs latest frame and sets to img, out returns 0 if error and time if not error
         out, img1 = outputSink1.grabFrame(img1)
@@ -92,11 +100,16 @@ def main():
             
             continue
         
-        out, img1 = AprilDetector.detect(img1)
+        out, img1 = april.detect(img1)
 
         if out != 0:
-            translationPublisher.set(out[0])
-            rotationPublisher.set(out[1])
+            print(out)
+            translationXPublisher.set(value = out[0])
+            translationYPublisher.set(value = out[1])
+            translationZPublisher.set(value = out[2])
+            rotationXPublisher.set(value = out[3])
+            rotationYPublisher.set(value = out[4])
+            rotationZPublisher.set(value = out[5])
         
         #out, img2 = outputSink2.grabFrame(img2)
 
@@ -106,6 +119,8 @@ def main():
         #    continue
         
         #img2 = aprtags(img2)
+        
+        img1 = cv2.resize(img1, (360,240))
         outputSource.putFrame(img1)
 
 
